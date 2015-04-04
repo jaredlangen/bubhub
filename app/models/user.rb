df1cc403d9e52b5204b2ad06fbe61af9c3f1891c
Benny Mei <meibenny@gmail.com>
2015-04-04 01:14:06 -0300

    made more RESTful and can save and access users from db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 
-  attr_accessor :first_name, :last_name, :b_mail, :b_number, :pin, :phone_number, :bu_card_number
+#attr_accessible :first_name, :last_name, :email, :bu_id, :pin
 
 end
