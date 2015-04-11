83d6a145d5858bf094390f689e66a56fe9fe42d4
Benny Mei <meibenny@gmail.com>
2015-04-11 19:28:04 -0300

    Users model has a valid factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  it "has a valid factory"
+  it "has a valid factory" do
+    FactoryGirl.create(:user).should be_valid
+  end
   it "is invalid without a first name"
   it "is invalid without a last name"
   it "is invalid without an email address"
