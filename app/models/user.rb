844f1024b799605f2a38281051cd930dd727fee7
Benny Mei <meibenny@gmail.com>
2015-04-11 19:40:49 -0300

    added basic tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_uniqueness_of :email, :allow_blank => false, :case_sensitive => false
+  validates_presence_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_presence_of :pin
-  validates_uniqueness_of :bu_id, :allow_blank => false
+  validates_presence_of :bu_id
+  validates_uniqueness_of :bu_id
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,27 @@ require 'spec_helper'
 
 describe User do
   it "has a valid factory" do
+    #create builds an instance of User and saves it to the DB
     FactoryGirl.create(:user).should be_valid
   end
-  it "is invalid without a first name"
-  it "is invalid without a last name"
-  it "is invalid without an email address"
-  it "is invalid without a BU ID"
-  it "is invalid without a pin"
+  it "is invalid without a first name" do
+    #We use build here to create a new instance of User
+    #however, this new instance is not saved,
+    #bypassing the validation that is in the User model
+    #this is important to get the test to pass
+    FactoryGirl.build(:user, first_name: nil).should_not be_valid
+  end
+  it "is invalid without a last name" do
+    FactoryGirl.build(:user, last_name: nil).should_not be_valid
+  end
+  it "is invalid without an email address" do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+  it "is invalid without a BU ID" do
+    FactoryGirl.build(:user, bu_id: nil).should_not be_valid
+  end
+  it "is invalid without a pin" do
+    FactoryGirl.build(:user, pin: nil).should_not be_valid
+  end
   
 end
