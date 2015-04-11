74cd292640fdb3adc47d82295f6b4490722008c6
Benny Mei <meibenny@gmail.com>
2015-04-11 18:41:16 -0300

    added skeleton for model rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory"
+  it "is invalid without a first name"
+  it "is invalid without a last name"
+  it "is invalid without an email address"
+  it "is invalid without a BU ID"
+  it "is invalid without a pin"
+  
 end
