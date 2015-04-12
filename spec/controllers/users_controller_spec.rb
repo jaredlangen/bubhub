d457a46742979e890655d60570a54e6cb5dcf756
Benny Mei <meibenny@gmail.com>
2015-04-11 21:10:05 -0300

    Added users controller RSpec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe UsersController do
 
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new user in the database" do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to change(User, :count).by(1)
+      end
+      it "redirects to the home page after saving" do
+        post :create, user: FactoryGirl.attributes_for(:user)
+        response.should redirect_to root_page_path
+      end
+    end
+  end
+
 end
